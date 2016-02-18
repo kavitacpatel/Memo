0237352a17fb1c50f4906637a0701893bdd15cca
kavita patel <kavita_cpatel@yahoo.com>
2016-02-18 16:56:31 -0500

    core data

---
+++ Person+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Person+CoreDataProperties.swift
+//  Person
+//
+//  Created by kavita patel on 2/18/16.
+//  Copyright © 2016 kavita patel. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Person {
+
+    @NSManaged var name: String?
+
+}
---
+++ Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  Person
+//
+//  Created by kavita patel on 2/18/16.
+//  Copyright © 2016 kavita patel. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Person: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
